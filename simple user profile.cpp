c2f477941e48a318f619c3aee00812ca716aa976
ME9799 <93213577+ME9799@users.noreply.github.com>
2021-10-26 20:44:10 +0300

    Add files via upload

---
+++ simple user profile.cpp
@@ -0,0 +1,29 @@
+//simple user profile
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	cout<<"Capturing string datatype \n";
+	string fname ,lname,meal, movie ;
+	
+	cout<<"Input your first name: \n";
+	getline(cin,fname);
+	cout<<"Input your last name: \n";
+	getline(cin,lname);
+	cout<<"Input your favourite meal:  \n";
+	getline(cin,meal);
+	cout<<"Input your favourite movie: \n";
+	getline(cin,movie);
+	cout<<fname: ;
+	cout<<lname;
+	cout<<"\n";
+	cout<<meal;
+	cout<<"\n";
+	cout<<movie;
+	
+ 	
+	
+	return 0;
+}
